2115c6cdd8cb9d5ce85e25cd6654e837e7472716
Amund Myrbostad <Lullinj98@gmail.com>
2025-05-19 09:59:04 +0200

    Null safety on PrefillExtensions.cs

--- PrefillExtensions.cs
+++ PrefillExtensions.cs
@@ -15,58 +15,73 @@ public static class PrefillExtensions
                 dialog.Party = "urn:altinn:person:identifier-no:20815497741";
 
                 //Title
-                dialog.Content.Title.MediaType = "text/plain";
-                dialog.Content.Title.Value =
-                [
-                    new V1CommonLocalizations_Localization
-                    {
-                        Value = "Skattemeldingen for 2024",
-                        LanguageCode = "nb"
-                    }
-                ];
+                dialog.Content.Title = new V1CommonContent_ContentValue
+                {
+                    MediaType = "text/plain",
+                    Value =
+                    [
+                        new V1CommonLocalizations_Localization
+                        {
+                            Value = "Skattemeldingen for 2024",
+                            LanguageCode = "nb"
+                        }
+                    ]
+                };
 
                 //Summary
-                dialog.Content.Summary.MediaType = "text/plain";
-                dialog.Content.Summary.Value =
-                [
-                    new V1CommonLocalizations_Localization
-                    {
-                        Value = "Dette er skattemeldingen din for 2024. Informasjonen er delt med deg 2025-5-16 10:22",
-                        LanguageCode = "nb"
-                    }
-                ];
+                dialog.Content.Summary = new V1CommonContent_ContentValue
+                {
+                    MediaType = "text/plain",
+                    Value =
+                    [
+                        new V1CommonLocalizations_Localization
+                        {
+                            Value = "Dette er skattemeldingen din for 2024. Informasjonen er delt med deg 2025-5-16 10:22",
+                            LanguageCode = "nb"
+                        }
+                    ]
+                };
 
                 //Sender Name
-                dialog.Content.SenderName.MediaType = "text/plain";
-                dialog.Content.SenderName.Value =
-                [
-                    new V1CommonLocalizations_Localization
-                    {
-                        Value = "fogd og fut",
-                        LanguageCode = "nb"
-                    }
-                ];
+                dialog.Content.SenderName = new V1CommonContent_ContentValue
+                {
+                    MediaType = "text/plain",
+                    Value =
+                    [
+                        new V1CommonLocalizations_Localization
+                        {
+                            Value = "fogd og fut",
+                            LanguageCode = "nb"
+                        }
+                    ]
+                };
                 //Additional Info
-                dialog.Content.AdditionalInfo.MediaType = "text/plain";
-                dialog.Content.AdditionalInfo.Value =
-                [
-                    new V1CommonLocalizations_Localization
-                    {
-                        Value = "Dette teksten er tilleggsinformasjonen som ligger kun i denne dialogen.",
-                        LanguageCode = "nb"
-                    }
-                ];
+                dialog.Content.AdditionalInfo = new V1CommonContent_ContentValue
+                {
+                    MediaType = "text/plain",
+                    Value =
+                    [
+                        new V1CommonLocalizations_Localization
+                        {
+                            Value = "Dette teksten er tilleggsinformasjonen som ligger kun i denne dialogen.",
+                            LanguageCode = "nb"
+                        }
+                    ]
+                };
 
                 //Main content reference
-                dialog.Content.MainContentReference.MediaType = "application/vnd.dialogporten.frontchannelembed-url;type=text/markdown";
-                dialog.Content.MainContentReference.Value =
-                [
-                    new V1CommonLocalizations_Localization
-                    {
-                        Value = "https://dialogporten-serviceprovider-ahb4fkchhgceevej.norwayeast-01.azurewebsites.net/fce",
-                        LanguageCode = "nb"
-                    }
-                ];
+                dialog.Content.MainContentReference = new V1CommonContent_ContentValue
+                {
+                    MediaType = "application/vnd.dialogporten.frontchannelembed-url;type=text/markdown",
+                    Value =
+                    [
+                        new V1CommonLocalizations_Localization
+                        {
+                            Value = "https://dialogporten-serviceprovider-ahb4fkchhgceevej.norwayeast-01.azurewebsites.net/fce",
+                            LanguageCode = "nb"
+                        }
+                    ]
+                };
 
                 //SearchTags
                 dialog.SearchTags =
