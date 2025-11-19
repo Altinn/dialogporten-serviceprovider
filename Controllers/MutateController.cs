ac641aa1be6a62c282da4a9c09449ab5cbae34cf
Amund Myrbostad <Lullinj98@gmail.com>
2025-11-19 12:53:00 +0100

    Mutate Can have either DialogToken OR DialogId inside State. makes Bootstrapping new dialogs much easier

--- Controllers/MutateController.cs
+++ Controllers/MutateController.cs
@@ -1,18 +1,17 @@
+using System.Diagnostics.CodeAnalysis;
+using Altinn.ApiClients.Dialogporten;
 using Altinn.ApiClients.Dialogporten.Features.V1;
 using Digdir.BDB.Dialogporten.ServiceProvider.Playbook;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-
 
 
 namespace Digdir.BDB.Dialogporten.ServiceProvider.Controllers;
 
 [ApiController]
 [Route("mutate")]
-[Authorize]
 [EnableCors("AllowedOriginsPolicy")]
-public class MutateController(IServiceownerApi dialogporten) : ControllerBase
+public class MutateController(IServiceownerApi dialogporten, IDialogTokenValidator dialogTokenValidator) : ControllerBase
 {
 
     [HttpGet]
@@ -20,16 +19,28 @@ public class MutateController(IServiceownerApi dialogporten) : ControllerBase
     public async Task<IActionResult> MutatePlaybook(
         [FromRoute] string base64PlaybookState)
     {
-
         var compiler = new PlaybookCompiler
         {
             Progress = 0
         };
         var playbookState = await PlaybookState.DecodeFromBase64(base64PlaybookState);
+
         if (playbookState == null)
         {
             return new BadRequestResult();
         }
+
+        if (playbookState.DialogId == Guid.Empty)
+        {
+            var authHeader = Request.Headers.Authorization.FirstOrDefault();
+            if (!TryGetGuidFromToken(authHeader, out var guid))
+            {
+                return new BadRequestResult();
+            }
+
+            playbookState.DialogId = guid.Value;
+        }
+
 
         var dialog = await dialogporten.V1ServiceOwnerDialogsQueriesGetDialog(playbookState.DialogId, null!, CancellationToken.None);
         if (!dialog.IsSuccessful)
@@ -56,4 +67,31 @@ public class MutateController(IServiceownerApi dialogporten) : ControllerBase
     }
 
 
+    private bool TryGetGuidFromToken(string? token, [NotNullWhen(true)] out Guid? guid)
+    {
+        guid = null;
+
+        if (token == null || !token.StartsWith("Bearer "))
+        {
+            return false;
+        }
+
+        var jwtToken = token["Bearer ".Length..].Trim();
+        var result = dialogTokenValidator.Validate(jwtToken);
+
+        if (!result.IsValid)
+        {
+            return false;
+        }
+
+        var dialogIdClaim = result.ClaimsPrincipal.Claims.FirstOrDefault(c => c.Type == "i");
+        if (!Guid.TryParse(dialogIdClaim?.Value, out var dialogId))
+        {
+            return false;
+        }
+
+        guid = dialogId;
+        return true;
+
+    }
 }
